7ce4c3fcee4584e99444ad2cb9106f8d00501441
Maham Waseem <mahammahmmod887@gmail.com>
2026-07-17 00:44:26 +0500

    Coomon modules

---
+++ 04-modules-commonjs-usage.cjs
@@ -0,0 +1,23 @@
+// COMMONJS MODULE (IMPORT)
+// Import the exported values
+const {
+  PI,
+  APP_NAME,
+  circleArea,
+  circumference,
+  greet,
+  student,
+} = require("./04-modules-commonjs.cjs");
+
+// Use the imported constant
+console.log(APP_NAME);           // Javascript Learner
+console.log(PI);                 // 3.14
+
+// Use the imported functions
+console.log(greet("Maham"));     // Hello, Maham!
+console.log(circleArea(5));      // 78.5
+console.log(circumference(5));   // 31.4
+
+// Use the imported object
+console.log(student.name);       // Maham
+console.log(student.course);     // JavaScript
---
+++ 04-modules-commonjs.cjs
@@ -0,0 +1,37 @@
+// COMMONJS MODULE (EXPORT)
+
+// constants
+const PI = 3.14;
+
+const APP_NAME = "Javascript Learner";
+
+// Function to calculate the area of a circle
+function circleArea(radius) {
+  return PI * radius * radius;
+}
+
+// Function to calculate the circumference
+function circumference(radius) {
+  return 2 * PI * radius;
+}
+
+// Function to greet a user
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+// An object
+const student = {
+  name: "Maham",
+  course: "JavaScript",
+};
+
+// Export everything that other files should use
+module.exports = {
+  PI,
+  APP_NAME,
+  circleArea,
+  circumference,
+  greet,
+  student,
+};
